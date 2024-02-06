41b5cfadfe42e9b5cec0a950935a61e5b0e1cbbb
Chris <chris79gp@outlook.com>
2024-02-06 13:18:19 +0000

    refactor: rename clearContent to removeChildElement and add a parameter to define which parent element to remove child elements from

--- src/modules/index.js
+++ src/modules/index.js
@@ -7,23 +7,21 @@ const hero = createHeroSection();
 const banner = createBanner();
 const about = createAboutSection();
 
-const clearContent = function() {
-    while (contentContainer.firstChild) {
-        contentContainer.removeChild(contentContainer.lastChild);
+const removeChildElements = function(parentElement) {
+    while (parentElement.firstChild) {
+        parentElement.removeChild(parentElement.lastChild);
     }
 }
 
 const navButtons = document.querySelector('nav').children;
 
-for (let i = 0; i < navButtons.length; i++) {
-    navButtons[i].addEventListener('mousedown', clearContent);
-}
-
 navButtons[0].addEventListener('mousedown', () => {
+    removeChildElements(contentContainer)
     contentContainer.appendChild(hero);
 });
     
 navButtons[1].addEventListener('mousedown', () => {
+    removeChildElements(contentContainer)
     contentContainer.appendChild(banner);
     contentContainer.appendChild(about);
 })
