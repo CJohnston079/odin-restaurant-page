964dc95d956ae06d9c007b06c0d47b739f898292
Chris <chris79gp@outlook.com>
2024-02-09 12:33:20 +0000

    feat(book-a-table.js): give wrap inputs in a parent form element

--- src/modules/book-a-table/book-a-table.js
+++ src/modules/book-a-table/book-a-table.js
@@ -4,42 +4,49 @@ const createBookSection = function() {
     const section = document.createElement("section");
     section.id = "book-a-table";
 
+    const form = document.createElement("form");
+    
+    // Name input
+    const nameInput = document.createElement('input');
+    nameInput.type = "text";
+    nameInput.placeholder = "Name";
+    form.appendChild(nameInput);
+
     // Date input
     const dateInput = document.createElement('input');
     dateInput.type = "date";
     dateInput.placeholder = "Date";
-    section.appendChild(dateInput);
+    form.appendChild(dateInput);
 
     // Time input
     const timeInput = document.createElement('input');
     timeInput.type = "time";
     timeInput.placeholder = "Time";
-    section.appendChild(timeInput);
+    form.appendChild(timeInput);
 
     // Number of guests input
     const guestsInput = document.createElement('input');
     guestsInput.type = "number";
     guestsInput.placeholder = "Number of Guests";
-    section.appendChild(guestsInput);
-
-    // Name input
-    const nameInput = document.createElement('input');
-    nameInput.placeholder = "Name";
-    section.appendChild(nameInput);
+    form.appendChild(guestsInput);
 
     // Email input
     const emailInput = document.createElement('input');
     emailInput.type = "email";
     emailInput.placeholder = "Email Address";
-    section.appendChild(emailInput);
+    form.appendChild(emailInput);
 
     // Submit button
     const submitButton = document.createElement('button');
+    submitButton.type = "submit";
     submitButton.textContent = "Submit";
-    section.appendChild(submitButton);
+    form.appendChild(submitButton);
+
+    section.appendChild(form);
 
     return section;
 }
+
 
 
 export default createBookSection;
