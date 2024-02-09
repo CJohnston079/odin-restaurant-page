48125d2d168aac33ea4151700142356269646747
Chris <chris79gp@outlook.com>
2024-02-09 12:30:01 +0000

    feat(book-a-table.js): add code to generate input fields

--- src/modules/book-a-table/book-a-table.js
+++ src/modules/book-a-table/book-a-table.js
@@ -4,7 +4,42 @@ const createBookSection = function() {
     const section = document.createElement("section");
     section.id = "book-a-table";
 
+    // Date input
+    const dateInput = document.createElement('input');
+    dateInput.type = "date";
+    dateInput.placeholder = "Date";
+    section.appendChild(dateInput);
+
+    // Time input
+    const timeInput = document.createElement('input');
+    timeInput.type = "time";
+    timeInput.placeholder = "Time";
+    section.appendChild(timeInput);
+
+    // Number of guests input
+    const guestsInput = document.createElement('input');
+    guestsInput.type = "number";
+    guestsInput.placeholder = "Number of Guests";
+    section.appendChild(guestsInput);
+
+    // Name input
+    const nameInput = document.createElement('input');
+    nameInput.placeholder = "Name";
+    section.appendChild(nameInput);
+
+    // Email input
+    const emailInput = document.createElement('input');
+    emailInput.type = "email";
+    emailInput.placeholder = "Email Address";
+    section.appendChild(emailInput);
+
+    // Submit button
+    const submitButton = document.createElement('button');
+    submitButton.textContent = "Submit";
+    section.appendChild(submitButton);
+
     return section;
 }
+
 
 export default createBookSection;
