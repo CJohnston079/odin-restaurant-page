aada9d1b11340aee5ce528d33c10c85fa07eecd4
Chris <chris79gp@outlook.com>
2024-02-11 12:12:41 +0000

    test(getNextTwoWeeks.test.js): add test that first value of getNextTwoWeeks should be today's date

--- __tests__/utils/getNextTwoWeeks.test.js
+++ __tests__/utils/getNextTwoWeeks.test.js
@@ -6,4 +6,9 @@ describe('getNextTwoWeeks', () => {
         expect(Array.isArray(result)).toBe(true);
         expect(result.length).toBe(14);
     });
+    it("should return today's date as the first item", () => {
+        const result = getNextTwoWeeks();
+        const today = new Date();
+        expect(result[0].getDate()).toEqual(today.getDate());
+    });
 });
