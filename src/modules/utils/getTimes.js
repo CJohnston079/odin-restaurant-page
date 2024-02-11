f9102de2afe6729e7511e0fcb9caab03bf748927
Chris <chris79gp@outlook.com>
2024-02-11 21:18:22 +0000

    feat(getTimes.js): add getTimes() getTimes() generates an array of times in the format hh:mm it adds times at 30-minute intervals between 12:00 and 13:30 and between 17:00 and 19:30

--- src/modules/utils/getTimes.js
+++ src/modules/utils/getTimes.js
@@ -1,5 +1,13 @@
 const getTimes = function() {
-    return;
+    const times = [];
+    for (let i = 12; i < 20; i += 1) {
+        if (i >= 14 && i < 17) {
+            continue;
+        }
+        times.push(i + ':00');
+        times.push(i + ':30');
+    };
+    return times;
 };
 
 export default getTimes;
