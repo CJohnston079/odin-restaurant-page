67e8452383b31b689b84f322b170853f932527c2
Chris <chris79gp@outlook.com>
2024-02-11 12:18:45 +0000

    feat(getNextTwoWeeks.js): add getNextTwoWeeks() function

--- src/modules/utils/getNextTwoWeeks.js
+++ src/modules/utils/getNextTwoWeeks.js
@@ -1,5 +1,14 @@
 function getNextTwoWeeks() {
-    return;
+    let dates = [];
+    let today = new Date();
+
+    for (let i = 0; i < 14; i++) {
+        let date = new Date(today);
+        date.setDate(today.getDate() + i);
+        dates.push(date);
+    }
+
+    return dates;
 }
 
 export default getNextTwoWeeks;
