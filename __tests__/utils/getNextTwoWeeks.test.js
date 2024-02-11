e8ae364d1d327d60c48b6a5eb1bf573c16a18a86
Chris <chris79gp@outlook.com>
2024-02-11 12:04:25 +0000

    test(getNextTwoWeeks.test.js): add test that return value of getNextTwoWeeks() has 14 items

--- __tests__/utils/getNextTwoWeeks.test.js
+++ __tests__/utils/getNextTwoWeeks.test.js
@@ -1,1 +1,9 @@
 import getNextTwoWeeks from "../../src/modules/utils/getNextTwoWeeks";
+
+describe('getNextTwoWeeks', () => {
+    it("should return an array with 14 items", () => {
+        const result = getNextTwoWeeks();
+        expect(Array.isArray(result)).toBe(true);
+        expect(result.length).toBe(14);
+    });
+});
