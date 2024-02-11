2092fd424b1b2e3b1c76643940c9d544bf522015
Chris <chris79gp@outlook.com>
2024-02-11 12:17:12 +0000

    test(getNextTwoWeeks.test.js): add test that last value of getNextTwoWeeks is the date two weeks from today

--- __tests__/utils/getNextTwoWeeks.test.js
+++ __tests__/utils/getNextTwoWeeks.test.js
@@ -11,4 +11,14 @@ describe('getNextTwoWeeks', () => {
         const today = new Date();
         expect(result[0].getDate()).toEqual(today.getDate());
     });
+    it('should return dates for the next two weeks', () => {
+        const result = getNextTwoWeeks();
+        const latestDate = result[result.length - 1];
+        
+        const today = new Date();
+        const twoWeeksLater = new Date(today);
+        twoWeeksLater.setDate(today.getDate() + 13);
+
+        expect(latestDate.getDate()).toBe(twoWeeksLater.getDate());
+    });
 });
