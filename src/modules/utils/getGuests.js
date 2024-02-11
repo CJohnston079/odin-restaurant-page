7cce41f306c0eb63b27ce61fb4165ad34dd8efc8
Chris <chris79gp@outlook.com>
2024-02-11 22:02:26 +0000

    feat(getGuests.js): add getGuests() takes a parameter guests which defaults to 2 returns an array which suffixes each number up to 'guests' with 'people' suffixes the first guest with 'person'

--- src/modules/utils/getGuests.js
+++ src/modules/utils/getGuests.js
@@ -1,5 +1,15 @@
-const getGuests = function() {
-    return;
+const getGuests = function(guests=2) {
+    const guestOptions = ['1 person'];
+
+    if (guests < 2) {
+        return guestOptions;
+    }
+
+    for (let i = 2; i <= guests; i += 1) {
+        guestOptions.push(`${i} people`);
+    };
+
+    return guestOptions;
 };
 
 export default getGuests;
