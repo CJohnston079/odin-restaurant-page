a4060dd0bd41e0237234202ce9d99686cd23604b
Chris <chris79gp@outlook.com>
2024-02-20 15:54:11 +0000

    test(menuItems.test.js): add testing suite to check properties are set correctly by createFoodItem()

--- __tests__/menuItems.test.js
+++ __tests__/menuItems.test.js
@@ -12,4 +12,15 @@ describe('createFoodItem', () => {
         expect(testFood.hasOwnProperty('price')).toBe(true);
         expect(testFood.hasOwnProperty('ingredients')).toBe(true);
     });
+    it('should set properties inherited from createMenuItems', () => {
+        expect(testFood.name).toBe('Sandwich');
+        expect(testFood.price).toBe(5.99);
+        expect(testFood.ingredients).toEqual(['bread', 'butter']);
+    });
+    it('should set type property to "food"', () => {
+        expect(testFood.type).toBe('food');
+    });
+    it('should set isVegetarian property', () => {
+        expect(testFood.isVegetarian).toBe(true);
+    });
 });
