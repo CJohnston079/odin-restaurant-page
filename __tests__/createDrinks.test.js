79db19fbba1aa0d1660726cbe67e03b1b3521dfc
Chris <chris79gp@outlook.com>
2024-02-22 15:48:57 +0000

    test(createDrinks.test.js): add unit test for property arguments of createWine()

--- __tests__/createDrinks.test.js
+++ __tests__/createDrinks.test.js
@@ -1,7 +1,19 @@
 import { createWine } from "../src/modules/menu/createDrinks";
 
 describe('createWine', () => {
-    it('should return', () => {
-        expect(createWine()).toBe('');
+    const testWine = createWine({
+        name: 'trebbiano',
+        bottlePrice: 19.95,
+        country: 'italy',
+        description: 'easy drinking, crisp, apple',
+        wineType: 'white'
+    });
+    it('should return an object with provided properties', () => {
+        expect(testWine.name).toBe('trebbiano');
+        expect(testWine.price).toEqual({ '175ml': 5.95, '250ml': 7.95, '750ml': 19.95 });
+        expect(testWine.country).toBe('italy');
+        expect(testWine.description).toBe('easy drinking, crisp, apple');
+        expect(testWine.drinkType).toBe('wine');
+        expect(testWine.wineType).toBe('white');
     });
 });
