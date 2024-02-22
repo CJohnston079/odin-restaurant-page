a07063060481d7b7623350825c9b862a5ad92c73
Chris <chris79gp@outlook.com>
2024-02-22 16:30:09 +0000

    feat(createDrinks.js): add createWine() factory function createWine() extends createDrink it sets the price property as an object of sizes and corresponding prices it returns drinkType: 'wine', wineType, country and description

--- src/modules/menu/createDrinks.js
+++ src/modules/menu/createDrinks.js
@@ -1,7 +1,22 @@
 import { createDrink } from "./menuItems";
 
-const createWine = function() {
-    return '';
+const createWine = function({
+    name,
+    bottlePrice,
+    country,
+    description,
+    wineType
+}) {
+    const newWine = createDrink({
+        name,
+        price: {
+            '175ml': Math.ceil(bottlePrice * 0.3) - 0.05,
+            '250ml': Math.ceil(bottlePrice * 0.4) - 0.05,
+            '750ml': bottlePrice
+        }
+    });
+
+    return { ...newWine, drinkType: 'wine', wineType, country, description }
 }
 
 export { createWine };
