af7e09b6038830232a0a879e5dcb3e85f1dfccf3
Chris <chris79gp@outlook.com>
2024-02-26 12:08:02 +0000

    style: format .babelrc and webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,38 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: 'development',
-    entry: {
-        index: './src/modules/index.js',
-        hero: ['./src/modules//hero/hero.js', './src/modules/hero/hero.css'],
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-        static: './src',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-            filename: 'index.html',
-            inject: 'head',
-            scriptLoading: 'defer',
-        })
-    ],
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
-    optimization: {
-        runtimeChunk: 'single',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-        ],
-    },
+	mode: "development",
+	entry: {
+		index: "./src/modules/index.js",
+		hero: ["./src/modules//hero/hero.js", "./src/modules/hero/hero.css"],
+	},
+	devtool: "inline-source-map",
+	devServer: {
+		static: "./src",
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./src/index.html",
+			filename: "index.html",
+			inject: "head",
+			scriptLoading: "defer",
+		}),
+	],
+	output: {
+		filename: "[name].bundle.js",
+		path: path.resolve(__dirname, "dist"),
+		clean: true,
+	},
+	optimization: {
+		runtimeChunk: "single",
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader"],
+			},
+		],
+	},
 };
