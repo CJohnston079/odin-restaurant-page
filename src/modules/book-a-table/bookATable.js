0c59be50a2f5a2999cfeba4e820afc43a3b73d95
Chris <chris79gp@outlook.com>
2024-02-28 15:00:52 +0000

    refactor: abstract logic for closing dialog element to closeDialog()

--- src/modules/book-a-table/bookATable.js
+++ src/modules/book-a-table/bookATable.js
@@ -10,17 +10,9 @@ const createBookDialog = function () {
 	const closeButton = createButton("×");
 
 	closeButton.classList.add("close");
-	dialog.appendChild(closeButton);
-	closeButton.addEventListener("mousedown", () => {
-		dialog.classList.remove("dialog-open");
-		setTimeout(() => {
-			dialog.classList.add("dialog-closed");
-		}, 10);
-		setTimeout(() => {
-			dialog.close();
-		}, 610);
-	});
+	closeButton.addEventListener("mousedown", () => closeDialog(dialog));
 
+	dialog.appendChild(closeButton);
 	dialog.appendChild(form);
 
 	return dialog;
@@ -106,6 +98,18 @@ const createLabelledDropdown = function (name, labelText, options) {
 const setEmailPattern = function (input) {
 	input.pattern =
 		"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)])";
+};
+
+const closeDialog = function (dialog) {
+	dialog.classList.remove("dialog-open");
+
+	setTimeout(() => {
+		dialog.classList.add("dialog-closed");
+	}, 10);
+
+	setTimeout(() => {
+		dialog.close();
+	}, 610);
 };
 
 export default createBookDialog;
