98a6b5c7e486cac10315bfd698c2f2003e17ae33
Chris <chris79gp@outlook.com>
2024-03-04 21:29:18 +0000

    build: add createFood.js and createFood.test.js

---
+++ __tests__/createFood.test.js
@@ -0,0 +1,11 @@
+// import {
+// 	createStarter,
+// 	createSide,
+// 	createMain,
+// 	createDessert,
+// } from "../src/modules/menu/createFood";
+
+describe("createStarter", () => {});
+describe("createSide", () => {});
+describe("createMain", () => {});
+describe("createDessert", () => {});
---
+++ src/modules/menu/createFood.js
@@ -0,0 +1,19 @@
+// import { createFood } from "./menuItems";
+
+const createStarter = function () {
+	return;
+};
+
+const createSide = function () {
+	return;
+};
+
+const createMain = function () {
+	return;
+};
+
+const createDessert = function () {
+	return;
+};
+
+export { createStarter, createSide, createMain, createDessert };
