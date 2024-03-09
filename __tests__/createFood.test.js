98b53f5a2436e75cd26adbe66c3f3ae6852c1271
Chris <chris79gp@outlook.com>
2024-03-09 22:17:26 +0000

    test: add unit tests for createStarter()

--- __tests__/createFood.test.js
+++ __tests__/createFood.test.js
@@ -1,11 +1,31 @@
 import {
-	// createStarter,
+	createStarter,
 	createSide,
 	// createMain,
 	// createDessert,
 } from "../src/modules/menu/createFood";
 
-describe("createStarter", () => {});
+describe("createStarter", () => {
+	const testStarter = createStarter({
+		name: "pesto pomodoro bread",
+		price: 5.95,
+		allergens: { isVegetarian: true },
+		description: "brushed with garlic and parsley butter",
+		course: "starter",
+	});
+	it("should return a starter course object with provided properties", () => {
+		expect(testStarter.name).toBe("pesto pomodoro bread");
+		expect(testStarter.price).toBe(5.95);
+		expect(testStarter.allergens).toEqual({
+			isGlutenFree: false,
+			isVegetarian: true,
+			isVegan: false,
+		});
+		expect(testStarter.description).toBe("brushed with garlic and parsley butter");
+		expect(testStarter.course).toBe("starter");
+		expect(testStarter.isSpecial).toBe(false);
+	});
+});
 describe("createSide", () => {
 	const testSide = createSide({
 		name: "sautéed garlic vegetables",
@@ -14,7 +34,7 @@ describe("createSide", () => {
 		description: "french beans, tenderstem broccoli & chantenay carrots",
 		course: "side",
 	});
-	it("should return a new wine object with provided properties", () => {
+	it("should return a new side dish object with provided properties", () => {
 		expect(testSide.name).toBe("sautéed garlic vegetables");
 		expect(testSide.price).toBe(4.95);
 		expect(testSide.allergens).toEqual({ isGlutenFree: true, isVegetarian: false, isVegan: true });
