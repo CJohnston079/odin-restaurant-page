6d4ee94a612e6a7eefd7e95c1f0633697e895365
Chris <chris79gp@outlook.com>
2024-03-09 22:17:57 +0000

    feat: add createStarter() createStarter() extends createFood() to return a new instance of a 'starter' object

--- src/modules/menu/createFood.js
+++ src/modules/menu/createFood.js
@@ -1,7 +1,8 @@
 import { createFood } from "./menuItems";
 
-const createStarter = function () {
-	return;
+const createStarter = function ({ name, price, description, allergens = {}, isSpecial = false }) {
+	const newSide = createFood({ name, price, allergens });
+	return { ...newSide, course: "starter", description, isSpecial };
 };
 
 const createSide = function ({ name, price, description, allergens = {}, isSpecial = false }) {
