d0e5b6d94e9f10158c9d8b5738c234c11dfcfa8f
Chris <chris79gp@outlook.com>
2024-03-11 21:28:01 +0000

    feat: 'calories' property is now returned by createStarter(), createSide(), createMain() and createDessert()

--- src/modules/menu/createFood.js
+++ src/modules/menu/createFood.js
@@ -1,23 +1,51 @@
 import { createFood } from "./menuItems";
 
-const createStarter = function ({ name, price, description, allergens = {}, isSpecial = false }) {
+const createStarter = function ({
+	name,
+	price,
+	description,
+	calories,
+	allergens = {},
+	isSpecial = false,
+}) {
 	const newSide = createFood({ name, price, allergens });
-	return { ...newSide, course: "starter", description, isSpecial };
+	return { ...newSide, course: "starter", description, calories, isSpecial };
 };
 
-const createSide = function ({ name, price, description, allergens = {}, isSpecial = false }) {
+const createSide = function ({
+	name,
+	price,
+	description,
+	calories,
+	allergens = {},
+	isSpecial = false,
+}) {
 	const newSide = createFood({ name, price, allergens });
-	return { ...newSide, course: "side", description, isSpecial };
+	return { ...newSide, course: "side", description, calories, isSpecial };
 };
 
-const createMain = function ({ name, price, description, allergens = {}, isSpecial = false }) {
+const createMain = function ({
+	name,
+	price,
+	description,
+	calories,
+	allergens = {},
+	isSpecial = false,
+}) {
 	const newMain = createFood({ name, price, allergens });
-	return { ...newMain, course: "main", description, isSpecial };
+	return { ...newMain, course: "main", description, calories, isSpecial };
 };
 
-const createDessert = function ({ name, price, description, allergens = {}, isSpecial = false }) {
+const createDessert = function ({
+	name,
+	price,
+	description,
+	calories,
+	allergens = {},
+	isSpecial = false,
+}) {
 	const newDessert = createFood({ name, price, allergens });
-	return { ...newDessert, course: "dessert", description, isSpecial };
+	return { ...newDessert, course: "dessert", description, calories, isSpecial };
 };
 
 export { createStarter, createSide, createMain, createDessert };
