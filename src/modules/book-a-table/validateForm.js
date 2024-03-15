8942f0851d2574169742a24f04cf2953a0b7f66b
Chris <chris79gp@outlook.com>
2024-03-15 12:30:44 +0000

    feat: add getRequiredInputs() which returns all form inputs that have the required attribute

--- src/modules/book-a-table/validateForm.js
+++ src/modules/book-a-table/validateForm.js
@@ -3,23 +3,33 @@ const setEmailPattern = function (input) {
 		"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)])";
 };
 
-// const checkValueMissing = function () {};
+// const checkRequiredInputs = function () {};
 
-// const checkPattern = function () {};
+// const checkPatternInputs = function () {};
 
 const checkFormIsValid = function (form) {
-	const nameInput = form.children[3].lastChild;
-	const emailInput = form.children[4].lastChild;
+	// const nameInput = form.children[1].lastChild;
+	// const emailInput = form.children[4].lastChild;
+	const requiredInputs = getRequiredInputs(form);
 
-	if (nameInput.validity.valueMissing || emailInput.validity.valueMissing) {
-		alert("please fill in all fields");
-		return;
-	}
+	requiredInputs.forEach(input => {
+		console.log(input.textContent);
+		if (input.validity.valueMissing) {
+			alert("please fill in all fields");
+			return;
+		}
+	});
 
-	if (!emailInput.checkValidity()) {
-		alert("please enter a vaid email address");
-		return;
-	}
+	// if (!emailInput.checkValidity()) {
+	// 	alert("please enter a vaid email address");
+	// 	return;
+	// }
+};
+
+const getRequiredInputs = function (form) {
+	return Array.from(form.children)
+		.map(child => child.lastChild)
+		.filter(input => input.required);
 };
 
 export { setEmailPattern, checkFormIsValid };
