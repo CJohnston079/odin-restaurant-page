4cc0e81396d65b9c08eaf1d1a18da0213b160d31
Chris <chris79gp@outlook.com>
2024-03-15 21:43:15 +0000

    refactor: abstract logic to check validity of form inputs

--- src/modules/book-a-table/validateForm.js
+++ src/modules/book-a-table/validateForm.js
@@ -3,25 +3,12 @@ const setEmailPattern = function (input) {
 		"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)])";
 };
 
-// const checkRequiredInputs = function () {};
-
-// const checkEmailInput = function () {};
-
 const checkFormIsValid = function (form) {
 	const requiredInputs = getRequiredInputs(form);
 	const emailInput = form.querySelector("input[type='email']");
 
-	requiredInputs.forEach(input => {
-		if (input.validity.valueMissing) {
-			alert("please fill in all fields");
-			return;
-		}
-	});
-
-	if (!emailInput.checkValidity()) {
-		alert("please enter a valid input");
-		return;
-	}
+	checkRequiredInputs(requiredInputs);
+	checkEmailInput(emailInput);
 };
 
 const getRequiredInputs = function (form) {
@@ -29,6 +16,22 @@ const getRequiredInputs = function (form) {
 		.map(child => child.lastChild)
 		.slice(0, -1)
 		.filter(input => input.required);
+};
+
+const checkRequiredInputs = function (requiredInputs) {
+	requiredInputs.forEach(input => {
+		if (input.validity.valueMissing) {
+			alert("please fill in all fields");
+			return;
+		}
+	});
+};
+
+const checkEmailInput = function (emailInput) {
+	if (!emailInput.checkValidity()) {
+		alert("please enter a valid input");
+		return;
+	}
 };
 
 export { setEmailPattern, checkFormIsValid };
