8bfd4d93dda70a6da6c1a656938b3fbaf71bd0e1
Chris <chris79gp@outlook.com>
2024-03-19 12:44:32 +0000

    feat: add displayFormSubmittedMessage() which logs a booking message to the console on successful form submission

--- src/modules/book-a-table/submitForm.js
+++ src/modules/book-a-table/submitForm.js
@@ -2,12 +2,20 @@ const submitForm = function (form) {
 	const formData = {
 		date: form.querySelector("select[name='date']").value,
 		time: form.querySelector("select[name='time']").value,
-		number: form.querySelector("select[name='number']").value,
+		numOfPeople: form.querySelector("select[name='number']").value,
 		fullName: form.querySelector("input[name='full-name']").value,
 		email: form.querySelector("input[name='email-address']").value,
 	};
 
+	displayFormSubmittedMessage(formData);
+
 	return formData;
+};
+
+const displayFormSubmittedMessage = function (data) {
+	const bookingMessage = `Your table as been booked for ${data.numOfPeople} on ${data.date} at ${data.time}.`;
+
+	console.log(bookingMessage);
 };
 
 export { submitForm };
