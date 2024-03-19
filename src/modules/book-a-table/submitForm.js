ae1b2e04322b1d2689a1b7d205edf8eca13faf1a
Chris <chris79gp@outlook.com>
2024-03-19 12:39:59 +0000

    refactor: store form data in submitForm() as an object instead of individual variables

--- src/modules/book-a-table/submitForm.js
+++ src/modules/book-a-table/submitForm.js
@@ -1,16 +1,13 @@
 const submitForm = function (form) {
-	const date = form.querySelector("select[name='date']").value;
-	const time = form.querySelector("select[name='time']").value;
-	const number = form.querySelector("select[name='number']").value;
-	const fullName = form.querySelector("input[name='full-name']").value;
-	const email = form.querySelector("input[name='email-address']").value;
+	const formData = {
+		date: form.querySelector("select[name='date']").value,
+		time: form.querySelector("select[name='time']").value,
+		number: form.querySelector("select[name='number']").value,
+		fullName: form.querySelector("input[name='full-name']").value,
+		email: form.querySelector("input[name='email-address']").value,
+	};
 
-	console.log("Date:", date);
-	console.log("Time:", time);
-	console.log("Number of guests:", number);
-	console.log("Full name:", fullName);
-	console.log("Email address:", email);
-	return form;
+	return formData;
 };
 
 export { submitForm };
