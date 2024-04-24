ee7b5151ad0e5750fb0a215f0434bd9675c45b69
Chris <chris79gp@outlook.com>
2024-04-24 17:27:26 +0100

    feat: add renderMenuItem which takes a menuItem object and renders its name and price in the DOM

--- src/modules/menu/menu.js
+++ src/modules/menu/menu.js
@@ -1,8 +1,43 @@
 import "./menu.css";
+import {
+	createWine,
+	// createCocktail,
+	// createSpirit,
+	// createBottledDrink,
+	// createSoftDrink,
+	// createHotDrink,
+} from "./createDrinks";
+
+const testWine = createWine({
+	name: "chardonay",
+	bottlePrice: 19.95,
+	allergens: { isVegan: true },
+	country: "chile",
+	description: "fruity, tropical fruits",
+	wineType: "white",
+});
+
+const renderMenuItem = function (item) {
+	const container = document.createElement("div");
+	const name = document.createElement("h3");
+	const price = document.createElement("p");
+
+	name.textContent = item.name;
+	price.textContent = item.price["750ml"];
+
+	container.append(name);
+	container.append(price);
+
+	return container;
+};
 
 const createMenuSection = function () {
 	const section = document.createElement("section");
 	section.id = "menu";
+
+	const testMenuItem = renderMenuItem(testWine);
+
+	section.append(testMenuItem);
 
 	return section;
 };
