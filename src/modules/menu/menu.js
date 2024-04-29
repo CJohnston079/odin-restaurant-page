e263d80734b3134527b2197790948a5e08f52178
Chris <chris79gp@outlook.com>
2024-04-29 22:06:14 +0100

    feat: add rose wines to menu

--- src/modules/menu/menu.js
+++ src/modules/menu/menu.js
@@ -4,6 +4,7 @@ import { wines } from "./createMenuItems.js";
 
 const whiteWineData = wines.filter(wine => wine.wineType === "white");
 const redWineData = wines.filter(wine => wine.wineType === "red");
+const roseWineData = wines.filter(wine => wine.wineType === "rosé");
 
 const createMenuSection = function () {
 	const section = document.createElement("section");
@@ -15,11 +16,17 @@ const createMenuSection = function () {
 	const redWinesHeading = createMenuSubheading("Red Wines");
 	const redWines = renderMenuItems(redWineData);
 
+	const roseWinesHeading = createMenuSubheading("Rosé Wines");
+	const roseWines = renderMenuItems(roseWineData);
+
 	section.append(whiteWinesHeading);
 	section.append(whiteWines);
 
 	section.append(redWinesHeading);
 	section.append(redWines);
+
+	section.append(roseWinesHeading);
+	section.append(roseWines);
 
 	return section;
 };
