8fee8f15d1c72b6637e505975cae82c66c97826c
Chris <chris79gp@outlook.com>
2024-04-29 21:50:19 +0100

    feat: render red wines on menu page

--- src/modules/menu/menu.js
+++ src/modules/menu/menu.js
@@ -2,15 +2,24 @@ import "./menu.css";
 import renderMenuItems from "./renderMenuItems.js";
 import { wines } from "./createMenuItems.js";
 
+const whiteWineData = wines.filter(wine => wine.wineType === "white");
+const redWineData = wines.filter(wine => wine.wineType === "red");
+
 const createMenuSection = function () {
 	const section = document.createElement("section");
 	section.id = "menu";
 
 	const whiteWinesHeading = createMenuSubheading("White Wines");
-	const whiteWines = renderMenuItems(wines);
+	const whiteWines = renderMenuItems(whiteWineData);
+
+	const redWinesHeading = createMenuSubheading("Red Wines");
+	const redWines = renderMenuItems(redWineData);
 
 	section.append(whiteWinesHeading);
 	section.append(whiteWines);
+
+	section.append(redWinesHeading);
+	section.append(redWines);
 
 	return section;
 };
